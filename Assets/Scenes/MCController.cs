19b3285cbce4b9c5da3d065a93cf4076de3f906e
Leia0820 <angse-wk25@student.tarc.edu.my>
2026-08-27 02:39:45 +0800

    Animator control and added scripts

--- Assets/Scenes/MCController.cs
+++ Assets/Scenes/MCController.cs
@@ -5,6 +5,7 @@ public class MCController : MonoBehaviour
     float xInput;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private int Speed;
+    [SerializeField] private int CrouchSpeed;
     [SerializeField] private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,11 +17,27 @@ public class MCController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // A/D movement
         xInput = Input.GetAxis("Horizontal");
-        rb.linearVelocityX = xInput * Speed;
+
+        // Check if W is being held
+        bool isCrouching = Input.GetKey(KeyCode.W);
+
+        // Normal Walk
+        if (isCrouching)
+        {
+            rb.linearVelocityX = xInput * Speed;
+        }
+        // Crouch Walk
+        else
+        {
+            rb.linearVelocityX = xInput * CrouchSpeed;
+        }
+
 
         //Character animation
         animator.SetFloat("Speed", Mathf.Abs(xInput));
+        animator.SetBool("Crouch", isCrouching);
 
     }
 }
